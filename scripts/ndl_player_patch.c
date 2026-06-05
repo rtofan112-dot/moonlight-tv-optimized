3bea1be6697fa91c6377138a39eae3642aab1945
rtofan112-dot <r.tofan112@gmail.com>
2026-06-05 11:31:47 +0500

    Apply low latency optimizations and statistics breakout for LG QNED TV

---
+++ scripts/ndl_player_patch.c
@@ -0,0 +1,151 @@
+#include "ndl_common.h"
+
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+#include "opus_empty.h"
+
+static SS4S_PlayerContext *CreatePlayerContext(SS4S_Player *player);
+
+static void DestroyPlayerContext(SS4S_PlayerContext *context);
+
+static void PlayerSetWaitAudioVideoReady(SS4S_PlayerContext *context, bool option);
+
+static int OpusFeedEmpty(void *arg, const unsigned char *data, size_t size);
+
+const SS4S_PlayerDriver SS4S_NDL_webOS5_PlayerDriver = {
+    .Create = CreatePlayerContext,
+    .Destroy = DestroyPlayerContext,
+    .SetWaitAudioVideoReady = PlayerSetWaitAudioVideoReady,
+};
+
+static int UnloadMedia(SS4S_PlayerContext *context);
+
+static int LoadMedia(SS4S_PlayerContext *context);
+
+static void LoadCallback(int type, long long numValue, const char *strValue);
+
+static SS4S_PlayerContext *ActivatePlayerContext = NULL;
+
+int SS4S_NDL_webOS5_ReloadMedia(SS4S_PlayerContext *context) {
+    SS4S_NDL_webOS5_Log(SS4S_LogLevelInfo, "NDL", "Reloading media");
+    if (UnloadMedia(context) != 0) {
+        return -1;
+    }
+    return LoadMedia(context);
+}
+
+int SS4S_NDL_webOS5_UnloadMedia(SS4S_PlayerContext *context) {
+    return UnloadMedia(context);
+}
+
+uint64_t SS4S_NDL_webOS5_GetPts(const SS4S_PlayerContext *context) {
+    (void)context;
+    // Возвращаем 0 для отключения встроенной A/V буферизации и синхронизации webOS
+    return 0;
+}
+
+static SS4S_PlayerContext *CreatePlayerContext(SS4S_Player *player) {
+    assert(ActivatePlayerContext == NULL);
+    SS4S_PlayerContext *created = calloc(1, sizeof(SS4S_PlayerContext));
+    created->player = player;
+    ActivatePlayerContext = created;
+    return created;
+}
+
+static void DestroyPlayerContext(SS4S_PlayerContext *context) {
+    UnloadMedia(context);
+    free(context);
+    assert(context == ActivatePlayerContext);
+    ActivatePlayerContext = NULL;
+}
+
+static void PlayerSetWaitAudioVideoReady(SS4S_PlayerContext *context, bool option) {
+    context->waitAudioVideoReady = option;
+}
+
+static int UnloadMedia(SS4S_PlayerContext *context) {
+    int ret = 0;
+    if (context->mediaLoaded) {
+        SS4S_NDL_webOS5_Log(SS4S_LogLevelInfo, "NDL", "Unloading media");
+        context->mediaLoaded = false;
+        ret = NDL_DirectMediaUnload();
+    }
+    return ret;
+}
+
+static int LoadMedia(SS4S_PlayerContext *context) {
+    int ret;
+    if (!SS4S_NDL_webOS5_Initialized) {
+        SS4S_NDL_webOS5_Log(SS4S_LogLevelInfo, "NDL", "Initializing NDL");
+        if ((ret = NDL_DirectMediaInit(getenv("APPID"), NULL)) != 0) {
+            SS4S_NDL_webOS5_Log(SS4S_LogLevelError, "NDL", "Failed to init: ret=%d, error=%s", ret,
+                                NDL_DirectMediaGetError());
+            return ret;
+        }
+        SS4S_NDL_webOS5_Log(SS4S_LogLevelInfo, "NDL", "NDL_DirectMediaInit succeeded");
+        SS4S_NDL_webOS5_Initialized = true;
+    }
+    assert(SS4S_NDL_webOS5_Initialized);
+    assert(!context->mediaLoaded);
+    NDL_DIRECTMEDIA_DATA_INFO_T info = context->mediaInfo;
+    if (info.video.type == 0 && info.audio.type == 0) {
+        SS4S_NDL_webOS5_Log(SS4S_LogLevelWarn, "NDL", "LoadMedia but audio and video has no type");
+        return -1;
+    } else if (context->waitAudioVideoReady && (info.video.type == 0 || info.audio.type == 0)) {
+        SS4S_NDL_webOS5_Log(SS4S_LogLevelInfo, "NDL", "Defer LoadMedia because audio or video has no type");
+        return 0;
+    }
+    SS4S_NDL_webOS5_Log(SS4S_LogLevelInfo, "NDL", "NDL_DirectMediaLoad(video=%u (%d*%d), atype=%u)", info.video.type,
+                        info.video.width, info.video.height, info.audio.type);
+    if ((ret = NDL_DirectMediaLoad(&info, LoadCallback)) != 0) {
+        SS4S_NDL_webOS5_Log(SS4S_LogLevelWarn, "NDL", "NDL_DirectMediaLoad returned %d: %s", ret,
+                            NDL_DirectMediaGetError());
+        return ret;
+    }
+    if (context->mediaInfo.audio.type == NDL_AUDIO_TYPE_PCM) {
+        unsigned short empty_buf[8] = {0};
+        int numChannels = 2;
+        if (strncmp(context->mediaInfo.audio.pcm.channelMode, "mono", 4) == 0) {
+            numChannels = 1;
+        } else if (strncmp(context->mediaInfo.audio.pcm.channelMode, "6-channel", 10) == 0) {
+            numChannels = 6;
+        }
+        size_t size = numChannels * sizeof(unsigned short);
+        NDL_DirectAudioPlay(empty_buf, size, (long long) SS4S_NDL_webOS5_GetPts(context));
+    } else if (context->opusEmpty != NULL) {
+        SS4S_OpusEmptyFeed(context->opusEmpty, OpusFeedEmpty, context);
+    }
+
+    context->mediaLoaded = true;
+    clock_gettime(CLOCK_MONOTONIC, &context->mediaLoadedTime);
+    return ret;
+}
+
+static void LoadCallback(int type, long long numValue, const char *strValue) {
+    switch (type) {
+        case 0x16: {
+            SS4S_NDL_webOS5_Log(SS4S_LogLevelInfo, "NDL", "%s STATE_UPDATE_LOADCOMPLETED: %s", __FUNCTION__, strValue);
+            break;
+        }
+        case 0x17: {
+            SS4S_NDL_webOS5_Log(SS4S_LogLevelInfo, "NDL", "%s STATE_UPDATE_UNLOADCOMPLETED: %s", __FUNCTION__,
+                                strValue);
+            break;
+        }
+        case 0x1a: {
+            SS4S_NDL_webOS5_Log(SS4S_LogLevelInfo, "NDL", "%s STATE_UPDATE_PLAYING: %s", __FUNCTION__, strValue);
+            break;
+        }
+        default: {
+            SS4S_NDL_webOS5_Log(SS4S_LogLevelInfo, "NDL", "%s type=0x%02x, numValue=0x%llx, strValue=%p", __FUNCTION__,
+                                type, numValue, strValue);
+            break;
+        }
+    }
+}
+
+static int OpusFeedEmpty(void *arg, const unsigned char *data, size_t size) {
+    return NDL_DirectAudioPlay((void *) data, size, (long long) SS4S_NDL_webOS5_GetPts(arg));
+}
